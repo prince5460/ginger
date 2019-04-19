f35de2924304015dc1a602925bc7bab56cb24093
zhou <prince5460@126.com>
2019-04-19 17:58:21 +0800

    简化验证代码

--- app/api/v1/client.py
+++ app/api/v1/client.py
@@ -4,10 +4,9 @@
 @Date : 19-4-18 下午4:12
 @Desc :
 '''
-from flask import request
 
 from app.libs.enums import ClientTypeEnum
-from app.libs.error_code import ClientTypeError
+from app.libs.error_code import Success
 from app.libs.redprint import Redprint
 from app.models.user import User
 from app.validators.forms import ClientForm, UserEmailForm
@@ -17,19 +16,17 @@ api = Redprint('client')
 
 @api.route('/register', methods=['POST'])
 def create_client():
-    data = request.json
-    form = ClientForm(data=data)
-    form.validate_for_api()
+    # data = request.json
+    form = ClientForm().validate_for_api()
     promise = {
         ClientTypeEnum.USER_EMAIL: __register_user_by_email,
     }
     promise[form.type.data]()
-    return 'success'
+    return Success()
 
 
 def __register_user_by_email():
-    form = UserEmailForm(data=request.json)
-    if form.validate():
-        User.register_by_email(form.nickname.data,
-                               form.account.data,
-                               form.secret.data)
+    form = UserEmailForm().validate_for_api()
+    User.register_by_email(form.nickname.data,
+                           form.account.data,
+                           form.secret.data)
--- app/validators/base.py
+++ app/validators/base.py
@@ -4,16 +4,19 @@
 @Date : 19-4-19 下午5:04
 @Desc :
 '''
+from flask import request
 from wtforms import Form
 
 from app.libs.error_code import ParameterException
 
 
 class BaseForm(Form):
-    def __init__(self, data):
+    def __init__(self):
+        data = request.json
         super(BaseForm, self).__init__(data=data)
 
     def validate_for_api(self):
         valid = super(BaseForm, self).validate()
         if not valid:
             raise ParameterException(msg=self.errors)
+        return self
