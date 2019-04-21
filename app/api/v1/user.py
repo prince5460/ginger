79512ed021d40cdd73abe932ff846c2d483f859b
ZHOU <prince5460@126.com>
2019-04-21 18:12:49 +0800

    重写get_or_404和first_or_404

--- app/api/v1/user.py
+++ app/api/v1/user.py
@@ -4,15 +4,20 @@
 @Date : 19-4-17 下午4:19
 @Desc :
 '''
+from app.libs.error_code import NotFound
 from app.libs.redprint import Redprint
 from app.libs.token_auth import auth
+from app.models.user import User
 
 api = Redprint('user')
 
 
-@api.route('', methods=['GET'])
+@api.route('/<int:uid>', methods=['GET'])
 @auth.login_required
-def get_user():
+def get_user(uid):
+    user = User.query.get_or_404(uid)
+    # if not user:
+    #     raise NotFound()
     return 'Hello World!'
 
 
