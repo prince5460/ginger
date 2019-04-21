83fc082416b4492bdd7b5ffcf318d5db8cd11536
ZHOU <prince5460@126.com>
2019-04-21 11:42:04 +0800

    全局异常处理

--- ginger.py
+++ ginger.py
@@ -4,9 +4,30 @@
 @Date : 19-4-17 下午3:50
 @Desc :
 '''
+from werkzeug.exceptions import HTTPException
+
 from app.app import create_app
+from app.libs.error import APIException
+from app.libs.error_code import ServerError
 
 app = create_app()
 
+
+@app.errorhandler(Exception)
+def framework_error(e):
+    if isinstance(e, APIException):
+        return e
+    if isinstance(e, HTTPException):
+        code = e.code
+        msg = e.description
+        error_code = 1007
+        return APIException(msg, code, error_code)
+    else:
+        if not app.config['DEBUG']:
+            return ServerError()
+        else:
+            raise e
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
